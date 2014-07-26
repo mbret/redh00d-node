3d1299a6731216b4f973335421fc8c26aebbf51f
maxime bret <xmax54@mail.com>
2014-07-26 14:20:14 +0200

    .

---
+++ api/policies/rejectAll.js
@@ -0,0 +1,18 @@
+/**
+ * isAuthenticated
+ *
+ * @module      :: Policy
+ * @description :: Simple policy to allow any authenticated user.
+ *                 Assumes that your login action in one of your controllers sets `req.session.authenticated = true;`.
+ *                 The policy check simply if user is authenticated (with passport). In case of user is not logged then we give a chance
+ *                 to user to be authenticated with basic strategy. Finally we reject user.
+ * @docs        :: http://sailsjs.org/#!documentation/policies, https://github.com/jaredhanson/passport
+ *
+ */
+var passport = require('passport');
+
+module.exports = function(req, res, next) {
+
+    return res.forbidden( res.i18n("You cannot access this resource") );
+
+};
