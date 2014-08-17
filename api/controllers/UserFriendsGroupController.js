0e2ef7d30a8bdec4e9b8c9d0ecc03ec68893ed35
unknown <xmax54@gmail.com>
2014-08-17 21:59:34 +0200

    a

---
+++ api/controllers/UserFriendsGroupController.js
@@ -0,0 +1,30 @@
+/**
+ * Created by maxime on 30/07/2014.
+ * @description Interract with model UserfriendsGroup and UserGroupMember.
+ */
+module.exports = {
+
+    find: function(req, res){
+        return res.send(501);
+    },
+
+    findMultiple: function(req, res){
+        return res.send(501);
+    },
+
+    create: function(req, res){
+        return res.send(501);
+    },
+
+    addMember: function(req, res){
+        return res.send(501);
+    },
+
+    delete: function(req, res){
+        return res.send(501);
+    },
+
+    deleteMember: function(req, res){
+        return res.send(501);
+    }
+}
