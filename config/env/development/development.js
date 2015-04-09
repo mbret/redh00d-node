4eb33a720e33fd694ab3c521784de87708ea8a24
Maxime Bret <bret.maxime@gmail.com>
2015-04-09 13:37:28 +0200

    no message

--- config/env/development/development.js
+++ config/env/development/development.js
@@ -12,6 +12,8 @@
 
 module.exports = {
 
+    port: 1340,
+
   /***************************************************************************
    * Set the default database connection for models in the development       *
    * environment (see config/connections.js and config/models.js )           *
